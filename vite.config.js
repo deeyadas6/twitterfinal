a95ecc8456f54450df2ec623060b00cd91c3ed84
deeyadas6 <deeyadas6@gmail.com>
2025-03-27 21:27:53 +0530

    second  commit

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react-vendor";
-            if (id.includes("bootstrap")) return "bootstrap";
-            if (id.includes("mui")) return "mui-vendor";
-            return "vendor";
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Increase limit (optional)
-  },
-});
+  
+})
